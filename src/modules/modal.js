d888f00f1751d10a6b27b2d26d6e4654fb9e4d80
Vlad Vladimirov <blad20002000@mail.ru>
2022-03-21 00:47:22 +0700

    Сделал карусель услуг

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,6 +3,7 @@ const modal = () => {
     const modalOverlay = document.querySelector('.modal-overlay')
     const modalCallback = document.getElementById('callback')
     const modalClose = document.querySelector('.modal-close')
+    const servicesElements = document.querySelector('.services-elements')
 
     header.addEventListener('click', (e) => {
         e.preventDefault()
@@ -22,6 +23,14 @@ const modal = () => {
     modalClose.addEventListener('click', () => {
         modalOverlay.style.display = 'none'
         modalCallback.style.display = 'none'
+    })
+
+    servicesElements.addEventListener('click', (e) => {
+        e.preventDefault()
+        if (e.target.nextElementSibling.closest('.img-wrapper')) {
+            modalOverlay.style.display = 'block'
+            modalCallback.style.display = 'block'
+        }
     })
 }
 
---
+++ src/modules/slider.js
@@ -0,0 +1,38 @@
+const slider = () => {
+    const servicesCarousel = document.querySelector('.services-carousel')
+    
+    const arrowLeft = document.querySelector('.arrow-left')
+    const arrowRight = document.querySelector('.arrow-right')
+
+    for (let i = 3; i < servicesCarousel.children.length; i++) {
+        servicesCarousel.children[i].style.display = 'none'
+    }
+
+    arrowRight.addEventListener('click', () => {
+        let elem, temp
+        setTimeout(function() {
+            servicesCarousel.style.cssText = 'transition: none;';
+            elem = servicesCarousel.firstElementChild;
+            temp = elem.cloneNode(true); 
+            servicesCarousel.appendChild(temp);
+            temp.style.display = 'none'
+            servicesCarousel.removeChild(elem)
+            servicesCarousel.children[2].style.display = ''
+        }, 250);
+    })
+
+    arrowLeft.addEventListener('click', () => {
+        let elem, temp
+        setTimeout(function() {
+            elem = servicesCarousel.lastElementChild
+            temp = elem.cloneNode(true)
+            servicesCarousel.children[2].style.display = 'none'
+            servicesCarousel.insertBefore(temp, servicesCarousel.firstElementChild)
+            servicesCarousel.children[0].style.display = ''
+            servicesCarousel.removeChild(elem)
+            servicesCarousel.style.cssText = 'transition: margin '+500+'ms ease;'
+        }, 250)
+    })
+}
+
+export default slider
